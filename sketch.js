71097a4c65163f4c45290bbc6a1afcd1111748db
SHAURYACODER <71553272+SHAURYACODER@users.noreply.github.com>
2021-01-20 17:33:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,9 +4,8 @@ var gameState = 0;
 var playerCount;
 
 var database;
-
+var allPlayers;
 var form, player, game;
-
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -15,7 +14,12 @@ function setup(){
   game.getState();
   game.start();
 }
-
-
 function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
 }
